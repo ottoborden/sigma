e3ae7632f3f88e64f8b9e09f8df4bcfbf7e1ff9c
Otto Borden <ottoborden@gmail.com>
2017-10-15 00:47:05 -0400

    Fix static files dir

--- sigma/settings.py
+++ sigma/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
-    ('node_modules', 'static/node_modules'),
+    ('node_modules', 'sigma/static/node_modules'),
 ]
 
 # Simplified static file serving.
